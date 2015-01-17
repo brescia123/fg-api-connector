4b5c73421c01623ee6a4646501ab20bb3fbef1c5
Giacomo Bresciani <brescia123@gmail.com>
2015-01-17 19:10:47 +0100

    fix

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var app = express();
 app.set('port', (process.env.PORT || 5000));
 
 app.get('/', function (req, res){
-	res.send('bella');
+	res.send.satus(200).('bella');
 });
 
 app.get('/update', function (req, res) {
