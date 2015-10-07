eac216e9885306519bc86d3da6b017fd4f05a28c
Giacomo Bresciani <brescia123@gmail.com>
2015-10-07 17:43:19 +0200

    fixes

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ require('./modules/endpoints.js')(app, kimono, fgApi, config, utils);
 app.set('port', (process.env.PORT || 5000));
 
 // Start server
-var server = app.listen(app.get('port'), 'localhost', function() {
+var server = app.listen(app.get('port'), function() {
 
 	var host = server.address().address;
 	var port = server.address().port;
